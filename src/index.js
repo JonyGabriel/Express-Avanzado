75377575c8ed0683cfedc547b70190e6adc58fa1
JonyGabriel <jonygabriiel@gmail.com>
2023-10-25 23:20:09 -0300

    Primer Commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import ProductRouter from "./router/product.routes.js";
+
+const app = express()
+const PORT = 8080
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use("/api/products", ProductRouter)
+
+app.listen(PORT, () => {
+  console.log(`Servidor express puerto ${PORT}`);
+})
